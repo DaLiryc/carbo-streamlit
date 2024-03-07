542372781386270ed083fe7c9f27da7912266089
Cyril Marchand <marchand.cyr@gmail.com>
2024-03-07 12:12:38 +0100

    Changes to be committed: 	new file:   .vscode/settings.json 	modified:   app.py

--- app.py
+++ app.py
@@ -5,7 +5,7 @@ import numpy as np
 import datetime
 
 #If api in the cloud please update the url with the url
-url = 'http://127.0.0.1:8000'
+url = 'https://cabotrack-qoz5nlx2ga-ew.a.run.app'
 
 st.markdown("# This is our version of the Carbotrack app! #")
     
@@ -17,4 +17,15 @@ Please be aware that our app and our models are still at an early stage and can 
 
 Please upload/take a picture and test our app and see how much dose of insuline you should take based on the picture of your food received!
 '''
-st.file_uploader('Photo of your meal', type=['png', 'jpg', 'jpeg'], accept_multiple_files=False, help='Upload your photo')
+uploaded_file = st.file_uploader('Photo of your meal', type=['png', 'jpg', 'jpeg'], accept_multiple_files=False, help='Upload your photo')
+        
+if st.button('Give me an insuline recomendation!'):
+    if uploaded_file is not None:
+        files = {'file': uploaded_file.getvalue()}
+        response = requests.post(url + '/predict', files=files)
+        if response.status_code == 200:
+            st.write("Here are the results: ", response.json())
+        else:
+            st.write("Error in the prediction, our model is still learning, sorry!")
+    else:
+        st.write("Please upload an image")
