062e4ff17bb9b6c760bf430dc209a944b924aefe
Cyril Marchand <marchand.cyr@gmail.com>
2024-03-08 11:50:59 +0100

    modified:   app.py

--- app.py
+++ app.py
@@ -3,6 +3,35 @@ import requests
 import pandas as pd
 import numpy as np
 import datetime
+import hmac
+import streamlit as st
+
+def check_password():
+    """Returns `True` if the user had the correct password."""
+
+    def password_entered():
+        """Checks whether a password entered by the user is correct."""
+        if hmac.compare_digest(st.session_state["password"], st.secrets["password"]):
+            st.session_state["password_correct"] = True
+            del st.session_state["password"]  # Don't store the password.
+        else:
+            st.session_state["password_correct"] = False
+
+    # Return True if the password is validated.
+    if st.session_state.get("password_correct", False):
+        return True
+
+    # Show input for password.
+    st.text_input(
+        "Password", type="password", on_change=password_entered, key="password"
+    )
+    if "password_correct" in st.session_state:
+        st.error("😕 Password incorrect")
+    return False
+
+
+if not check_password():
+    st.stop()  # Do not continue if check_password is not True.
 
 #If api in the cloud please update the url with the url
 url = 'https://cabotrack-qoz5nlx2ga-ew.a.run.app'
