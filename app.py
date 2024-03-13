ee4b1c21902d6fc0057ce374eaedbc1956a6fd28
Cyril Marchand <marchand.cyr@gmail.com>
2024-03-13 10:37:35 +0100

    modified:   app.py

--- app.py
+++ app.py
@@ -54,7 +54,7 @@ url = 'https://carbotrackv1-7xel7l3dia-ew.a.run.app/predict'
 
 st.markdown("# Welcome to the Carbotrack app! #")
 
-logo = Image.open('/home/daliryc/code/DaLiryc/carbo-streamlit/carbo-streamlit/logo.jpg')
+logo = Image.open('carbo-streamlit/app.py')
 st.image(logo, width=100)    
     
 '''
